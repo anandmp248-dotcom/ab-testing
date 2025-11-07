ae8431a625e81607e380abd0abad54a47eb47a35
anand-mp <anand@shastha.in>
2025-11-07 15:17:13 +0530

    iuiodh

--- next.config.js
+++ next.config.js
@@ -18,12 +18,6 @@ const nextConfig = {
     urls: {
       themeId: "#THEMEID#",
       contentSetId: "#CONTENTSETID#",
-      platformDevelop:
-        "https://fplatformdeveloper.skartio.app/platformdeveloperframe/api/110",
-      experienceUrl:
-        "https://experienceadmin.skartio.app/experiencemanagerframe/api/110/",
-      leadUrl: "https://flead.skartio.app/leadframe/api/110",
-      careerUrl: "https://fcareer.skartio.app/careerframe/api/110",
     },
     siteIdentity: {
       siteName: "",
@@ -42,11 +36,8 @@ const nextConfig = {
         in: { minValue: 10, maxValue: 10 },
         bh: { minValue: 8, maxValue: 8 },
       },
-      GOOGLE_CLIENT_ID:
-        "520147050501-ir980i68rsdtr088l6fo6g4n3pe01imk.apps.googleusercontent.com",
-      facebookAppId: "165111935489533",
+
       GTAG_ID: "G-G-0SHRDQ0X4E",
-      PIXEL_ID: "1275218956778093",
     },
   },
   images: {
